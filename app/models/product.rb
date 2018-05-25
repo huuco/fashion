89c501a627bec345552c68b41656ce0516b1a8d5
GiangPhan <phanbagiang2504@gmail.com>
2018-05-25 08:20:08 +0700

    search products and customs layout

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,12 @@ class Product < ApplicationRecord
   has_many :images
   belongs_to :brand
   belongs_to :category
+
+  def self.get_new_products
+    Product.limit(Settings.products.record_per_page).order "created_at DESC"
+  end
+
+  def self.search(search)
+    where("name LIKE ?", "%#{search}%" )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,12 @@ Rails.application.routes.draw do
     resources :addresses
     resources :shippings
   end
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
+  get "/checkout", to: "checkout#index"
+  get "/signup", to:"users#new"
+  get "/shopping_cart", to: "cart#shopping_cart"
+  get "search(/:search)", to: "search#index" , as: :search
+  root "products#index"
+  resources :products
 end
