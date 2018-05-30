26601aacbfb863b48a25bc8119381de5808cb23c
PhamKhanhDong <khanhdong95vip@gmail.com>
2018-05-30 11:09:23 +0700

    frontend login logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,20 @@
 class SessionsController < ApplicationController
   def new
-    redirect_to admin_url if current_user.present? && current_user.admin?
+    if logged_in?
+      flash[:warning] = t ".logined"
+      redirect_user
+    end
   end
 
   def create
     user = User.find_by email: params[:session][:email].downcase
 
-    if user && user.authenticate(params[:session][:password]) && user.admin?
+    if user && user.authenticate(params[:session][:password])
       login user
       params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_to admin_url
+      redirect_user
     else
-      flash.now[:error] = t ".error_login"
+      flash.now[:danger] = t ".error_login"
       render :new
     end
   end
@@ -20,5 +23,15 @@ class SessionsController < ApplicationController
     log_out
     flash[:success] = t ".logout"
     redirect_to login_path
+  end
+
+  private
+
+  def redirect_user
+    if current_user.admin?
+      redirect_to admin_url
+    else
+      redirect_to root_url
+    end
   end
 end
