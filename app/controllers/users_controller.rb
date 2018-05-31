42a31b31518aca397b776d92be4e10d309c76bb1
PhamKhanhDong <khanhdong95vip@gmail.com>
2018-05-31 11:15:23 +0700

    my account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: %i(edit update)
+
   def new
     @user = User.new
   end
@@ -15,9 +17,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".save_success"
+    end
+    render :edit
+  end
+
   private
 
   def user_params
     params.require(:user).permit User::USER_PARAMS
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:warning] = t ".user_not_found"
+    redirect_to root_url
   end
 end
