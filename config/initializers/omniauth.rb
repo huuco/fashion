866e4f7ba119aa55bb6af0d7413da6e72ceff7b6
GiangPhan <phanbagiang2504@gmail.com>
2018-06-12 17:18:32 +0700

    Set login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "738468562380-io40e3b648noh4uon06utkboi3jav5fu.apps.googleusercontent.com", "z9hwfAkLsofswWa2Eb-lj0NE"
+  provider :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET']
 end
